27df9cd38b2ef16722ec6b23a10f672c5535b0f4
Ume-Aham Sylvester <117973007+gittybro12@users.noreply.github.com>
2024-07-26 20:35:29 +0100

    Update hOl.py

--- highOlow/hOl.py
+++ highOlow/hOl.py
@@ -1,19 +1,13 @@
 from gameData import data
 import random
-
-
 def randomSelect():
     ran = random.choice(data)
     return ran
-
-
 def is_higher(inp,ran1F,ran2F):
     if ran1F > ran2F:
         return inp == 'a'
     else:
-        return inp == 'b'
-    
-    
+        return inp == 'b'      
 score = 0
 ran2 = randomSelect()
 play = True
@@ -24,24 +18,13 @@ while play:
         ran2=randomSelect()
     print(f'your current score is {score}')
     print(f"{ran1['name']}, a {ran1['description']}, from {ran1['country']}")
-
     print("VS")
-
     print(f"{ran2['name']}, a {ran2['description']}, from {ran2['country']}")
-
-
     user_input = input("who has the most follows 'A' OR 'B'? ").lower()
     check = is_higher(user_input,ran1['follower_count'],ran2['follower_count'])
-
-
-
     if check:
         print("Thats right")
         score += 1
     else:
         play = False
         print(f"nice try but that was wrong, your score is: {score}")
-
-
-
-
